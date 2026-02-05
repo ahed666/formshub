30fc41e0e7cb62eabff22518b41c4c679b6257d0
ahed666 <ahed0naser0eddin@gmail.com>
2026-02-06 01:33:46 +0400

    update 2-6-26

--- resources/js/components/formOnDevice/EndPage.vue
+++ resources/js/components/formOnDevice/EndPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid justify-center items-center p-14"v-if="currentTranslation!=null">
-                <div class="flex justify-center items-center mb-8"><img class="h-auto w-48" src="../../../../public/images/deafult/success.svg" alt=""></div>
+                <div class="flex justify-center items-center mb-8"><img class="h-auto w-36" src="../../../../public/images/deafult/success.svg" alt=""></div>
         <div class="grid justify-center items-center">
                         <h1 class="font-bold leading-tight text-5xl select-none mb-5 text-center">{{currentTranslation.end_header}}</h1>
                         <h2 class="font-weight-bolder leading-tight text-3xl select-none text-center">{{currentTranslation.end_message}}</h2>
